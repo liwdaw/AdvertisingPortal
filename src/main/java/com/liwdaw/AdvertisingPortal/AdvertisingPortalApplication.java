69fa23a95bb7906d1d8ae532154654062f1ea826
liwdaw <dawid.liwocha@gmail.com>
2018-03-25 22:06:10 +0200

    project initialization

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/AdvertisingPortalApplication.java
@@ -0,0 +1,12 @@
+package com.liwdaw.AdvertisingPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingPortalApplication.class, args);
+	}
+}
