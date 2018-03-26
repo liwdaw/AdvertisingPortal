729f0ff7e599072cc1668099264dc2d11decfbb6
liwdaw <dawid.liwocha@gmail.com>
2018-03-26 12:58:10 +0200

    added SubCategory entity

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/entity/SubCategory.java
@@ -0,0 +1,31 @@
+package com.liwdaw.AdvertisingPortal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="sub_category")
+public class SubCategory {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="sub_category_id")
+    private int subCategoryID;
+    
+    @Column(name="name")
+    private String name;
+    
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+    
+}
