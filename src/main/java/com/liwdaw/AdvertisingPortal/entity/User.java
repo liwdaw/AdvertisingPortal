0bf1a5444937e0b821b7b0ca70cd2f34e3bf80b3
liwdaw <dawid.liwocha@gmail.com>
2018-03-26 12:40:26 +0200

    added User entity

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/entity/User.java
@@ -0,0 +1,43 @@
+package com.liwdaw.AdvertisingPortal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="user_id")
+    private int userID;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="password")
+    private String password;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Column(name="city")
+    private String city;
+    
+    @Column(name="phone_number")
+    private String phoneNumber;
+    
+    @Column(name="role")
+    private String role;
+    
+    @Column(name="enabled")
+    private boolean enabled;
+    
+}
