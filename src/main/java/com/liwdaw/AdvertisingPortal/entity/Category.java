e23c9ef7d07d5b380a156d9c0ebb0b7ac34ef265
liwdaw <dawid.liwocha@gmail.com>
2018-03-26 12:47:42 +0200

    added Category entity

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/entity/Category.java
@@ -0,0 +1,32 @@
+package com.liwdaw.AdvertisingPortal.entity;
+
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="category")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="category_id")
+    private int categoryID;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Lob
+    @Column(name="icon")
+    private Blob icon;
+    
+}
