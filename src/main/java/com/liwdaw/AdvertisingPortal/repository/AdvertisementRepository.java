1f0e3a9c301dc1fbfa0712f0b1282bc90b54c526
liwdaw <dawid.liwocha@gmail.com>
2018-04-16 10:49:58 +0200

    added repositories for every entity

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/repository/AdvertisementRepository.java
@@ -0,0 +1,9 @@
+package com.liwdaw.AdvertisingPortal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.liwdaw.AdvertisingPortal.entity.Advertisement;
+
+public interface AdvertisementRepository extends JpaRepository<Advertisement, Integer> {
+
+}
---
+++ src/main/java/com/liwdaw/AdvertisingPortal/repository/SubCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.liwdaw.AdvertisingPortal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.liwdaw.AdvertisingPortal.entity.SubCategory;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
+
+}
