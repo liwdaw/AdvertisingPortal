d730bf9fb97ef0339bab07f7a6af287e7cd87692
liwdaw <dawid.liwocha@gmail.com>
2018-04-16 10:37:24 +0200

    added Advertisement entity

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/entity/Advertisement.java
@@ -0,0 +1,51 @@
+package com.liwdaw.AdvertisingPortal.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="advertisement")
+public class Advertisement {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="advertisement_id")
+    private int advertisementID;
+    
+    @Column(name="title")
+    private String title;
+    
+    @Column(name="description")
+    private String description;
+    
+    @Column(name="price")
+    private int price;
+    
+    @Column(name="status")
+    private String status;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="add_date")
+    private Date addDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="refresh_Date")
+    private Date refreshDate;
+    
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+}
