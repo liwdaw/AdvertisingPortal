898b42a1ca0fd56e481d72af5e9abd8e44fdcdf1
liwdaw <dawid.liwocha@gmail.com>
2018-04-16 11:02:16 +0200

    added Service and Transactional annotations

--- src/main/java/com/liwdaw/AdvertisingPortal/service/CategoryService.java
+++ src/main/java/com/liwdaw/AdvertisingPortal/service/CategoryService.java
@@ -3,10 +3,14 @@ package com.liwdaw.AdvertisingPortal.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.liwdaw.AdvertisingPortal.entity.Category;
 import com.liwdaw.AdvertisingPortal.repository.CategoryRepository;
 
+@Transactional
+@Service
 public class CategoryService {
 
     @Autowired
