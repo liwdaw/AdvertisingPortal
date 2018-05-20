81e0200bde8c7662b5a7e95f704385ec6b1e9a29
liwdaw <dawid.liwocha@gmail.com>
2018-05-20 19:42:08 +0200

    changed return type from entity to DTO

--- src/main/java/com/liwdaw/AdvertisingPortal/service/CategoryService.java
+++ src/main/java/com/liwdaw/AdvertisingPortal/service/CategoryService.java
@@ -1,11 +1,13 @@
 package com.liwdaw.AdvertisingPortal.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.liwdaw.AdvertisingPortal.DTO.CategoryDTO;
 import com.liwdaw.AdvertisingPortal.entity.Category;
 import com.liwdaw.AdvertisingPortal.repository.CategoryRepository;
 
@@ -16,12 +18,15 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
     
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> getAllCategories() {
+        List<CategoryDTO> categories = new ArrayList<>();
+        categoryRepository.findAll().forEach(e->categories.add(new CategoryDTO(e)));
+        return categories;
     }
     
-    public Category getCategory(int id) {
-        return categoryRepository.findById(id).get();
+    public CategoryDTO getCategory(int id) {
+        CategoryDTO category = new CategoryDTO(categoryRepository.findById(id).get());
+        return category;
     }
     
 }
