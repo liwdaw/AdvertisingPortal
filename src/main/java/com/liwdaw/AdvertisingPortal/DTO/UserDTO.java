3755889922afe526cb88ef6bcbd923b28ed87578
liwdaw <dawid.liwocha@gmail.com>
2018-05-21 12:09:23 +0200

    added UserDTO

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/DTO/UserDTO.java
@@ -0,0 +1,31 @@
+package com.liwdaw.AdvertisingPortal.DTO;
+
+import com.liwdaw.AdvertisingPortal.entity.User;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+	private int id;
+	
+	private String email;
+	
+	private String name;
+	
+	private String city;
+	
+	private String phoneNumber;
+	
+	private String role;
+	
+	public UserDTO(User user) {
+		id = user.getId();
+		email = user.getEmail();
+		name = user.getName();
+		city = user.getCity();
+		phoneNumber = user.getPhoneNumber();
+		role = user.getRole();
+	}
+	
+}
