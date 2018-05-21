33426cb1e293d22ac64d58203b4ac4007766a94d
liwdaw <dawid.liwocha@gmail.com>
2018-05-21 11:51:50 +0200

    added methods for SubCategoryRepository

--- src/main/java/com/liwdaw/AdvertisingPortal/repository/SubCategoryRepository.java
+++ src/main/java/com/liwdaw/AdvertisingPortal/repository/SubCategoryRepository.java
@@ -1,9 +1,19 @@
 package com.liwdaw.AdvertisingPortal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.liwdaw.AdvertisingPortal.entity.SubCategory;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
 
+    public SubCategory findById(int id);
+    
+    public SubCategory findByName(String name);
+    
+    public List<SubCategory> findByCategoryId(int id);
+    
+    public List<SubCategory> findByCategoryName(String name);
+	
 }
