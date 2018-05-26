dfdd36c2579fe5587010a76c2307ece6e76d7216
liwdaw <dawid.liwocha@gmail.com>
2018-05-26 17:27:12 +0200

    added UserService

---
+++ src/main/java/com/liwdaw/AdvertisingPortal/service/UserService.java
@@ -0,0 +1,41 @@
+package com.liwdaw.AdvertisingPortal.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.liwdaw.AdvertisingPortal.dto.UserDTO;
+import com.liwdaw.AdvertisingPortal.entity.User;
+import com.liwdaw.AdvertisingPortal.repository.UserRepository;
+
+@Transactional
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    
+    public UserDTO getUserById(int id) {
+        UserDTO user = new UserDTO(repository.findById(id));
+        return user;
+    }
+    
+    public UserDTO getUserByEmailAndPassword(String email, String password) {
+        UserDTO user = new UserDTO(repository.findByEmailAndPassword(email, password));
+        return user;
+    }
+    
+    public List<UserDTO> getUsersByNameContaining(String name) {
+        List<UserDTO> users = new ArrayList<>();
+        repository.findByNameContainingIgnoreCase(name).forEach(e -> users.add(new UserDTO(e)));
+        return users;
+    }
+    
+    public void addUser(User user) {
+        repository.save(user);
+    }
+    
+}
