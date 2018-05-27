765d6bf3fcecc96572e61b3be76c15add885325b
liwdaw <dawid.liwocha@gmail.com>
2018-05-27 11:04:48 +0200

    added findById and findByAdvertisementId methods to Image repo

--- src/main/java/com/liwdaw/AdvertisingPortal/repository/ImageRepository.java
+++ src/main/java/com/liwdaw/AdvertisingPortal/repository/ImageRepository.java
@@ -1,9 +1,15 @@
 package com.liwdaw.AdvertisingPortal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.liwdaw.AdvertisingPortal.entity.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
+    public Image findById(int id);
+    
+    public List<Image> findByAdvertisementId(int id);
+    
 }
